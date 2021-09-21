f02859122ee10c19e2820fa6505a750d985728c0
Hendi Santika <hendisantika@gmail.com>
2021-09-21 08:02:56 +0700

    #7 - Add slow & super-slow API

--- src/main/java/com/hendisantika/controller/TestController.java
+++ src/main/java/com/hendisantika/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hendisantika.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,20 @@ public class TestController {
 
         Thread.sleep(20); // sleep for 20 milliseconds
         return "I'm fast!";
+    }
+
+    @GetMapping("/slow")
+    public String getSlowApi() throws InterruptedException {
+
+        Thread.sleep(3000); // sleep for 3 seconds
+        return "I'm slow :(";
+    }
+
+    @GetMapping("/super-slow")
+    @Profile({"predev", "dev", "staging"})
+    public String getSuperSlowApi() throws InterruptedException {
+
+        Thread.sleep(60000); // sleep for 1 minute!
+        return "I'm super slow. Refactor me before moving to production!! :)";
     }
 }

7794c33d799c5b5ef04fa6cf7a97672cffb3446d
Hendi Santika <hendisantika@gmail.com>
2021-09-21 07:55:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootElasticApmIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticApmIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElasticApmIntegrationApplication.class, args);
+    }
+
+}
